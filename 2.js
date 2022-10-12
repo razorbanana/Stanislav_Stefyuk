8421bf15ecf17a8022567498f59d5474d65baa75
razorbanana <stanislav.stefyuk2@gmail.com>
2022-10-12 15:09:17 +0300

    refactor: clearing reduce initial value

--- 2.js
+++ 2.js
@@ -2,7 +2,7 @@ const first_non_repeating_letter = word => word.split('')
 .reduce((result, x) => 
 word.indexOf(x) === word.lastIndexOf(x) 
 && (word.indexOf(x.toLowerCase()) === -1 || word.indexOf(x.toUpperCase()) === -1)
-&& result === '' ? result = x : result, result = '')
+&& result === '' ? result = x : result, '')
 
 console.log('"RrttFF"', `"${first_non_repeating_letter('RrttFF')}"`)
 console.log('"sTreSS"', `"${first_non_repeating_letter('sTreSS')}"`)
---
+++ 3.js
@@ -0,0 +1,1 @@
+const digital_root = num => num
