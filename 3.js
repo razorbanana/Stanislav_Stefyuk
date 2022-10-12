8567ee98448c14af8ebc28966ce0e307b421de0d
razorbanana <stanislav.stefyuk2@gmail.com>
2022-10-12 15:36:06 +0300

    feat: 3 task

--- 3.js
+++ 3.js
@@ -1,1 +1,8 @@
-const digital_root = num => num
+const digital_root = num => num.toString().split('').reduce((result, digit) => result += parseInt(digit), 0) > 9 
+? digital_root(num.toString().split('').reduce((result, digit) => result += parseInt(digit), 0))
+: num.toString().split('').reduce((result, digit) => result += parseInt(digit), 0)
+
+console.log('16', digital_root(16))
+console.log('942', digital_root(942))
+console.log('132189', digital_root(132189))
+console.log('493193', digital_root(493193))
